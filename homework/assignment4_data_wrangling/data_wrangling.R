0f20f4eaf9915ce5ae1a2f4be4713a28f2ed66dd
Sam Farmer <sam@MacBook-Pro.local>
2020-11-07 12:31:50 -0800

    Problem 1

---
+++ homework/assignment4_data_wrangling/data_wrangling.R
@@ -0,0 +1,19 @@
+# -------------------------------------------------------------------------
+# Author: Sam Farmer
+# date: 11/7/2020
+# assignment 4 data wrangling. This assignment will go over data frame manipulation
+# and creating new functions that will be used for future assignments
+# setup -------------------------------------------------------------------
+
+rm(list = ls())
+
+
+# problem 1 ---------------------------------------------------------------
+# Create a summarise_group_data function with args [data, group_id, obs, fun, ...]
+# the functil will return a dataframe with 'group_id' columnds and summary statistics
+
+summarise_group <- function(data, group_id, obs, fun, ...){
+  
+}
+
+
